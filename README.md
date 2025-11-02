d6daa8b0b9efe6053e3122b7c0d26bbef89d6610
Jessica Wang <wangjessica02@gmail.com>
2025-11-01 21:28:46 -0400

    Display image with Love

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+# How to execute code
+love src/
+```
---
+++ src/main.lua
@@ -0,0 +1,11 @@
+function love.load()
+	image = love.graphics.newImage('pika.png')
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+	love.graphics.draw(image, love.math.random(0,800), love.math.random(0,600))
+end
