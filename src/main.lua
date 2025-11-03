aac3403c56bc7fd63619198157a11da2c7455e58
Jessica Wang <wangjessica02@gmail.com>
2025-11-03 13:26:37 -0500

    Added libraries + objects

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,30 @@
+Object = require 'libraries/classic'
+Test = require 'objects/Test'
+
 function love.load()
 	image = love.graphics.newImage('pika.png')
+	local object_files = {}
+	recursiveEnumerate('objects', object_files)
+	requireFiles(object_files)
+end
+
+function recursiveEnumerate(folder, file_list) 
+	local items = love.filesystem.getDirectoryItems(folder)
+	for _, item in ipairs(items) do
+		local file = folder .. '/' .. item
+		if love.filesystem.isFile(file) then
+			table.insert(file_list, file)
+		elseif love.filesystem.isDirectory(file) then
+			recursiveEnumerate(file, file_list)
+		end
+	end
+end
+
+function requireFiles(files)
+	for _, file in ipairs(files) do
+		local file = file:sub(1, -5)
+		require(file)
+	end
 end
 
 function love.update(dt)
@@ -7,7 +32,7 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.draw(image, love.math.random(0,800), love.math.random(0,600))
+	love.graphics.draw(image, love.math.random(0,800), love.math.random(0,300))
 end
 
 function love.run()
